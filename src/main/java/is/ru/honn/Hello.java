92c3acd3a5bd6b00208a42e099f7642b87c9cf21
sverrir <sverrir1993@gmail.com>
2016-09-21 13:04:52 +0000

    initial commit

---
+++ src/main/java/is/ru/honn/Hello.java
@@ -0,0 +1,10 @@
+package is.ru.honn;
+
+/**
+ * Created by Sverrir on 21.9.2016.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
