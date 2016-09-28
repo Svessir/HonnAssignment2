24f1e2be3a59d893453d4fb1b0251971d64b4992
kari14@ru.is <karieiriks>
2016-09-28 14:12:30 +0000

    user and video

--- src/main/java/is/ru/honn/rutube/domain/Video.java
+++ src/main/java/is/ru/honn/rutube/domain/Video.java
@@ -20,12 +20,12 @@ import java.util.List;
  */
 public class Video {
 
-    int videoId;
-    String title;
-    String description;
-    String source;
-    String videoType;
-    List<String> tags;
+    private int videoId;
+    private String title;
+    private String description;
+    private String source;
+    private String videoType;
+    private List<String> tags;
 
     public Video(int videoId, String title, String description, String source, String videoType, List<String> tags) {
         this.videoId = videoId;
@@ -38,5 +38,45 @@ public class Video {
 
     public void addTag(String tag){
         tags.add(tag);
+    }
+
+    public int getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(int videoId) {
+        this.videoId = videoId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getVideoType() {
+        return videoType;
+    }
+
+    public void setVideoType(String videoType) {
+        this.videoType = videoType;
     }
 }
