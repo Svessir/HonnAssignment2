596112291e8f9c97567b9d1c88409f9ad18ff329
sverrir <sverrir1993@gmail.com>
2016-10-01 10:32:55 +0000

    test

--- src/main/java/is/ru/honn/rutube/service/UserServiceStub.java
+++ src/main/java/is/ru/honn/rutube/service/UserServiceStub.java
@@ -40,6 +40,12 @@ public class UserServiceStub implements UserService {
         return user.getUserId();
     }
 
+    /**
+     * Retrieves a single user from the service identified by the userId.
+     *
+     * @param userId The id of the user being retreived
+     * @return The user identified by the userId else null
+     */
     @Override
     public User getUser(int userId) {
         for(User us : userCollection){
@@ -50,22 +56,45 @@ public class UserServiceStub implements UserService {
         return null;
     }
 
+    /**
+     * Returns all Users maintained by the service.
+     *
+     * @return A list of users contained within the service
+     */
     @Override
     public List<User> getUsers() {
         return new ArrayList<>(userCollection);
     }
 
+    /**
+     * Registers observer to the service. If the observer is already registered
+     * to the service then this method does nothing.
+     *
+     * @param observer The observer that wants to register to the service's observer pool
+     */
     @Override
     public void registerObserver(Observer observer) {
         if(!observers.contains(observer))
             observers.add(observer);
     }
 
+    /**
+     * Unregisters an observer from the service. If the observer is not already registered
+     * to the service then this method does nothing.
+     *
+     * @param observer The observer that wants to unregister from the service's observer pool
+     */
     @Override
     public void unregisterObserver(Observer observer) {
         observers.remove(observer);
     }
 
+    /**
+     * Checks whether a user can be added to the service.
+     *
+     * @param user The user being added.
+     * @throws ServiceException If user cannot be added to the service.
+     */
     private void userInsertCheck(User user) throws ServiceException{
         if(user == null){
             throw new ServiceException("Could not add user to userService. Null cannot be added.");
@@ -77,6 +106,9 @@ public class UserServiceStub implements UserService {
         }
     }
 
+    /**
+     * Notifies all observers of changes relevant to observers.
+     */
     private void notifyObservers() {
         for(Observer observer : observers)
             observer.notifyObserver();
